ba07ab5a2fecc5d7f4a191d521dc69aaaacb466f
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2023-03-28 21:35:50 -0700

    refactoring styles for ButtonBar and Gallery components in App.css

--- src/ButtonBar.js
+++ src/ButtonBar.js
@@ -1,12 +1,11 @@
 function ButtonBar(props) {
     return (
-
         // TODO: Error Checking
         <div>
-            <button value={-5} onClick={props.handleIterate} style={{border: "none", margin: "0 5px", padding: "5px 10px"}}>&lt;&lt; Way Back</button>
-            <button value={-1} onClick={props.handleIterate} style={{border: "none", margin: "0 5px", padding: "5px 10px"}}>&lt; Back</button>
-            <button value={1} onClick={props.handleIterate} style={{border: "none", margin: "0 5px", padding: "5px 10px"}}>Next &gt;</button>
-            <button value={5} onClick={props.handleIterate} style={{border: "none", margin: "0 5px", padding: "5px 10px"}}>Big Next &gt;&gt;</button>
+            <button value={-5} onClick={props.handleIterate}>&lt;&lt; Way Back</button>
+            <button value={-1} onClick={props.handleIterate}>&lt; Back</button>
+            <button value={1} onClick={props.handleIterate}>Next &gt;</button>
+            <button value={5} onClick={props.handleIterate}>Big Next &gt;&gt;</button>
         </div>
     )
 }
--- src/Gallery.js
+++ src/Gallery.js
@@ -1,21 +1,21 @@
 function Gallery(props) {
     let primaryImg = (
-        <img src={props.objectImg} alt={props.title} style={{ "max-height": "70vh", "max-width": "90%" }} />
+        <img src={props.objectImg} alt={props.title} />
     )
 
     if (props.objectImg === "") {
         primaryImg = (
-            <div style={{ height: "200px", width: "300px", "box-shadow": "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)", display: "inline-flex", "align-items": "center", "justify-content": "center" }}>
-                <p style={{ "font-variant": "small-caps", "font-weight": "bold", "font-size": "20px" }}>No Image Available</p>
+            <div className="noImg">
+                <p>No Image Available</p>
             </div>
         )
     }
 
     return (
-        <div style={{ "background-color": "#e9e9ed", width: "90%", margin: "30px auto", "box-shadow": "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)" }}>
-            <h1 style={{ padding: "20px 50px 0", "font-variant": "small-caps", "font-size": "20px" }}>{props.title}</h1>
+        <div className="gallery">
+            <h1>{props.title}</h1>
             {primaryImg}
-            <p style={{ "font-variant": "small-caps", padding: "20px 0px 30px", }}>{props.artist}</p>
+            <p>{props.artist}</p>
         </div>
     )
 }
